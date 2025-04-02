666463d9e885f33302bb1f003fae8212fad819ac
s30882 <s30882@pjwstk.edu.pl>
2025-04-02 14:25:59 +0200

    First trial

---
+++ Tutorial4/Models/Dept.cs
@@ -0,0 +1,8 @@
+namespace Tutorial4.Models;
+
+public class Dept
+{
+    public int DeptNo { get; set; }
+    public string DName { get; set; }
+    public string Loc { get; set; }
+}
---
+++ Tutorial4/Program.cs
@@ -0,0 +1,9 @@
+namespace Tutorial4;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
